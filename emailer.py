7344c341202a947a32fe118dc93aad3965125f39
Shuaib Mohammad <shuaibm@gmail.com>
2014-09-24 15:30:19 -0400

    added exception handling, improved db locking

--- emailer.py
+++ emailer.py
@@ -5,21 +5,41 @@
 # 3. Update action with response from ultradox
 # 4. Extract links to any documents created by ultradox and update them to database
 
-import requests, psycopg2, json
+import requests, psycopg2, json, gspread
 from datetime import datetime
 import psycopg2.extras
+import httplib
 
 #Conguration Parameters
+
 db_config = {
- "host": "amazonaws.com", 
+ "host": "ec2-107-22-165-91.compute-1.amazonaws.com", 
  "port": "5432", 
- "dbname": "",
- "user": "",
- "password": ""
+ "dbname": "desvm4038gqi5j",
+ "user": "fehvvlrigocyeq",
+ "password": "r5p3V1zxwX28KHpuJqaDkGBPJf"
+}
+
+db_config_local_test = {
+ "host": "localhost", 
+ "port": "5433", 
+ "dbname": "sample",
+ "user": "postgres",
+ "password": "sample"
+}
+
+#Google Spreadsheets login
+gs_config = {
+    'email':'',
+    'password':''
 }
 
 #This is a contant required for correctly posting JSON requests
 HEADERS = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+class HTTPError(Exception):
+    def __init__(self,status_code):
+        self.status_code = status_code
 
 
 def post_data(url,data):
@@ -38,7 +58,8 @@ def post_data(url,data):
     if post.status_code == 200:
         return post.json()
     else:
-        return None
+        print post.headers
+        raise HTTPError(post.status_code)
 
 def get_doc_link(message):
     """
@@ -77,7 +98,56 @@ def create_test_action(cur,conn):
     cur.execute("INSERT INTO actions (customer_id, templateurl, action_parameters) VALUES (1,%s,%s)", [url,data])
     print cur.query
     conn.commit()
-    
+
+def read_invoices():
+    """
+    Reads Google Spreadsheet with next actions
+    Returns a list of dictionaries, where a dictionary is a record read from the spreadsheet
+    Created for test purposes, not intended to be used in production
+    """
+    try:
+        gc = gspread.login(gs_config['email'],gs['password'])
+        sh = gc.open_by_key('1G5ispFcCHw3ejVitV91xmdLFXhKOjn2E8JQ8fSmMCg0')
+        ws = sh.get_worksheet(0)
+        rows = ws.get_all_values()
+        cols = [col.replace(' ','').strip().lower() for col in rows.pop(0)]
+        data = [{k:v for k,v in zip(cols,row)} for row in rows]
+        action_rows = [row for row in data if len(row['customeremail'].strip()) and len(row['invoicecounter'].strip())]
+        print len(action_rows)
+        #if col in ultradox_cols
+        
+    except httplib.CannotSendRequest:
+        print "HTTP Error while reading invoice data from Google spreadsheet"
+        raise
+    except httplib.BadStatusLine:
+        print "HTTP Error while reading invoice data from Google spreadsheet"
+        raise
+
+    return [action_rows[1]]
+
+def write_invoices_to_db(action_rows):
+    """
+    Writes a list of dictionaries, each representing one invoice to the actions table
+    [{},{}, ...] -> None
+    Intended to be replaced by a database function
+    """
+    try:
+        INVOICE_URL = "http://www.ultradox.com/run?id=f615pzXmP0PM9oyT6HJsglcrTtWWyl"#"http://www.ultradox.com/run?id=3zEV2UR1dtp9Qsgd6Tytomr7f3pZt5"
+        TEMPLATE_COLS = ['amount', 'amountincludingvat', 'betalingsfristtekst', 'companyaddress', 'companyname', 'duedate', 'invoicecounter', 'servicetext', 'todaysdate', 'vat','customeremail']
+        conn = psycopg2.connect(" ".join("{0}='{1}'".format(k,v) for k,v in db_config.iteritems()))
+        psycopg2.extras.register_hstore(conn)
+        cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+        print len(action_rows)
+        for row in action_rows:
+            data = {k:v for k,v in row.iteritems() if k in TEMPLATE_COLS}
+            cur.execute("INSERT INTO actions (customer_id, templateurl, action_parameters) VALUES (%s,%s,%s)", [row['customerid'],INVOICE_URL,data])
+        conn.commit()
+    except:
+        print "DB Error while writing invoices to database"
+        raise
+    finally:
+        conn.close()
+        
 if __name__== '__main__':
     """
     This is the method that will be called whenever this script is invoked. It requires no input parameters
@@ -89,56 +159,75 @@ if __name__== '__main__':
             Read the JSON response from Ultradox and handle common error conditions
             Update the db row with success and response or failure
     """
-    #Connect to the database
-    conn = psycopg2.connect(" ".join("{0}='{1}'".format(k,v) for k,v in db_config.iteritems()))
-    #Configure the postgres connection to be able to handle python data objects in postgres hstores and arrays
-    psycopg2.extras.register_hstore(conn)
-    #Create cursor object with results to be returned in dictionary format
-    cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    try:
+        #Connect to the database
+        conn = psycopg2.connect(" ".join("{0}='{1}'".format(k,v) for k,v in db_config.iteritems()))
+        #Configure the postgres connection to be able to handle python data objects in postgres hstores and arrays
+        psycopg2.extras.register_hstore(conn)
+        #Create cursor object with results to be returned in dictionary format
+        cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
 
-    #Create some dummy test data
-    #create_test_action(cur,conn)
-    
-    cur.execute("""SELECT id,templateurl,action_parameters from actions WHERE completed is null and failed = false and locked=false""")
-    rows = cur.fetchall()
+        #Attempt to obtain and advisory lock, exit if a lock is already held.
+        cur.execute('SELECT pg_try_advisory_lock(987654321);')
+        if not cur.fetchone()['pg_try_advisory_lock']:
+            raise SystemExit(0)
+        #The above code would prevent the script to continue, if another instance of the script is already running
+        
+        
+        #Create some dummy test data
+        #create_test_action(cur,conn)
+        #write_invoices_to_db(read_invoices())
+        
+        cur.execute("""SELECT id,templateurl,action_parameters from actions WHERE completed is null and failed = false""")
+        
+        rows = cur.fetchall()
 
-    for record in rows:
-        #Store record id to be actioned in a temporary local variable rec_id
-        rec_id = record['id']
-        
-        #Mark the record as locked for transaction safety
-        cur.execute('UPDATE actions SET locked=true WHERE id=%s',[rec_id])
-        conn.commit()
-        
-        #Post JSON request to Ultradox       
-        response = post_data(record['templateurl'],record['action_parameters'])
-        
-        #Continue only if some response was received
-        if response:
-            #Check if an expected JSON response was recieved with 'steps' key, otherwise log failure
-            if response.has_key('steps'):
-                #Check returned response from Ultradox for any failure messages
-                l = [step['success'] for step in response['steps']]
-                if False in l:
-                    #if any failures were detected, record failure
-                    cur.execute('UPDATE actions SET locked=false, failed=true WHERE id=%s',[rec_id])
-                else:
-                    #No errors were encountered, process and update response to database
-                    doc_links = get_doc_link(response)
-                    cur.execute('UPDATE actions SET locked=false, failed=false, completed=now(), message_from_executor=%s, document_links=%s WHERE id=%s',[json.dumps(response),doc_links,rec_id])
-                    print "action_id %s successfully processed!" % str(rec_id)
-            else:
-                #Expected JSON response was not recieved, update database with failure flag
-                cur.execute('UPDATE actions SET locked=false, failed=true, message_from_executor=%s WHERE id=%s',[json.dumps(response),rec_id])
+        for record in rows:
+            #Store record id to be actioned in a temporary local variable rec_id
+            rec_id = record['id']
             
-        else:
-            #No response was recieved, log failure
-            cur.execute('UPDATE actions SET locked=false, failed=true WHERE id=%s',[rec_id])
+            #Post JSON request to Ultradox
+            try:
+                response = post_data(record['templateurl'],record['action_parameters'])            
+            
+                #Continue only if some response was received
+                if response:
+                    #Check if an expected JSON response was recieved with 'steps' key, otherwise log failure
+                    if response.has_key('steps'):
+                        #Check returned response from Ultradox for any failure messages
+                        l = [step['success'] for step in response['steps']]
+                        if False in l:
+                            #if any failures were detected, record failure
+                            cur.execute('UPDATE actions SET failed=true, message_from_executor=%s WHERE id=%s',[json.dumps(response),rec_id])
+                        else:
+                            #No errors were encountered, process and update response to database
+                            doc_links = get_doc_link(response)
+                            cur.execute('UPDATE actions SET failed=false, completed=now(), message_from_executor=%s, document_links=%s WHERE id=%s',[json.dumps(response),doc_links,rec_id])
+                            print "action_id %s successfully processed!" % str(rec_id)
+                    else:
+                        #Expected JSON response was not recieved, update database with failure flag
+                        cur.execute('UPDATE actions SET failed=true, message_from_executor=%s WHERE id=%s',[json.dumps(response),rec_id])
+                    
+                else:
+                    #No response was recieved, log failure
+                    cur.execute('UPDATE actions SET failed=true, message_from_executor=%s WHERE id=%s',['No Response Recieved',rec_id])
 
-        #Commit updates
+                #Commit updates
+                conn.commit()
+            #The following except clause handles any HTTP status code errors thrown by post_data and writes the message to database
+            except HTTPError as e:
+                print "HTTP Error, status code: %s" % str(e.status_code)
+                print "Headers:"
+                cur.execute('UPDATE actions SET failed=true, message_from_executor=%s WHERE id=%s',['HTTP ERROR STATUS CODE: %s' % str(e.status_code),rec_id])
+    except psycopg2.Error as e:
+        print e.pgcode
+        print e.pgerror
+        print e.diag.message_primary
+    finally:
+        #Release advisory lock, commit any pending transaction, close the database connection
+        cur.execute('SELECT pg_advisory_unlock(987654321);')
         conn.commit()
-    
-    conn.close()
+        conn.close()
 
 
                 
