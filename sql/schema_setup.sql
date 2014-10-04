3f80859fbc075f400d3707d4851b378cb47bc256
Shuaib Mohammad <shuaibm@gmail.com>
2014-10-04 13:11:45 -0400

    Added SQL scripts for setup and test data

---
+++ sql/schema_setup.sql
@@ -0,0 +1,23 @@
+/*
+This script is used to setup the database schema for automatic emailing service in python
+This configures the database to:
+	use hstore extentions
+	creates a sequence to be used for invoice id's that is supposed to be used in insert queries for invoices
+	creates a table to store actions that will be picked up by automatic emailer script
+*/
+
+CREATE EXTENSION hstore;
+
+CREATE SEQUENCE seq_invoice_id;
+
+CREATE TABLE actions (
+	id serial PRIMARY KEY, 
+	customer_id int, 
+	created TIMESTAMP, 
+	templateUrl varchar(9999), 
+	action_parameters hstore, 
+	completed TIMESTAMP(6), 
+	failed boolean NOT NULL DEFAULT FALSE, 
+	message_from_executor varchar(9999), 
+	document_links text[]
+	);
---
+++ sql/test_data.sql
@@ -0,0 +1,29 @@
+/* 
+The following insert query adds invoice actions to the actions table which will be picked up by the emailer script
+The customer id should be an integer
+templateURL is the integreation URL for an Ultradoc that expects a JSON input with input variables required by the Ultradoc
+action_parameters is a list of key/value pairs that will be sent a the JSON input to templateURL
+This can be a contatention of several hstore functions of the form hstore('invoicecounter',nextval('seq_invoice_id')::char)
+Note that both paramters to hstore should be strings and should use single quotes
+*/
+
+INSERT INTO actions (
+	customer_id,
+	templateurl,
+	action_parameters
+	) 
+VALUES (
+	997,
+	'http://www.ultradox.com/run?id=f615pzXmP0PM9oyT6HJsglcrTtWWyl',
+	'"vat"=>"1,700.00", 
+	"amount"=>"6,800.00", 
+	"duedate"=>"9/8/14", 
+	"todaysdate"=>"1/8/14", 
+	"companyname"=>"GOBUY ApS ", 
+	"servicetext"=>"Fuld Service - 2014:||Bogføring og udarbejdelse af moms- og årsopgørelse for 2014. |Samt udarbejdelse af skatteregnskab og indberetning til myndigheder.",
+	"customeremail"=>"changethis@email.com", 
+	"companyaddress"=>"Store Kongensgade 74 1 th|1264 København K ", 
+	"amountincludingvat"=>"8,500.00", 
+	"betalingsfristtekst"=>"Betaligsfrist message"' || 
+	hstore('invoicecounter',nextval('seq_invoice_id')::char)
+	);
